119db6472104a76b55df210672f644e11c75a389
jetlaim <nizar.septian@student.unmer.ac.id>
2022-04-11 11:02:00 +0800

    tambahan file baru css 3 dan PHP 1

---
+++ belajar-php/index.php
@@ -0,0 +1,21 @@
+<?php
+
+    // disini letak kode kita nantinya
+    $nama = 'SEAL';
+    $umur = '20';
+/*
+    echo $nama;
+    echo $umur;
+
+    echo 'halo anda siapa?';
+    echo "<br>";
+
+    define ("NAMA","ijay");
+
+    echo "nama kamu siapa?".NAMA;
+*/
+
+    $nama = array("ijay", "nijar", "jay");
+
+    echo "Halo Nama Saya ".$nama[0]." nama lain saya ".$nama[1]." bisa juga di panggil ".$nama[2];
+?>
